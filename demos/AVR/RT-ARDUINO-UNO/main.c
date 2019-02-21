5c4bdd58e637f9dbc7a5fd5085d53ecf543f78a1
joao <joaohccalbu@gmail.com>
2019-02-21 17:37:12 -0300

    adicionando codigo discutido na reunião pro Makefile, mcu & hal conf headers e aplicação de envio do mestre que está travando na placa

--- demos/AVR/RT-ARDUINO-UNO/main.c
+++ demos/AVR/RT-ARDUINO-UNO/main.c
@@ -16,20 +16,47 @@
 
 #include "ch.h"
 #include "hal.h"
+
+//adicionar as variaveis do Makefile conforme e-mail do prof Hermano
+#include "chprintf.h"
+
+#include <stdio.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+
 #include "hal_i2c.h"
+//#include "hal_i2cslave.h"
 /*
  * LED blinker thread, times are in milliseconds.
  */
+
+#define BUFFERSIZE 5
 static THD_WORKING_AREA(waThread1, 32);
 static THD_FUNCTION(Thread1, arg) {
+  i2caddr_t slaveaddr=5;
+  size_t txbytes=BUFFERSIZE;
+  size_t rxbytes=BUFFERSIZE;
+  uint8_t rxbuf[BUFFERSIZE];
+  uint8_t txbuffer[BUFFERSIZE] = "1234";
+  sysinterval_t TIMEOUT = 1000;
+  msg_t debug;
 
   (void)arg;
-  chRegSetThreadName("Blinker");
+  chRegSetThreadName("MasterSendI2C");
   while (true) {
     palTogglePad(IOPORT2, PORTB_LED1);
-    chThdSleepMilliseconds(1000);
-  }
+    chprintf((BaseSequentialStream *) &SD1, "iniciando processo de envio do master\r\n");
+    chThdSleepMilliseconds(500);
+    debug=i2cMasterTransmitTimeout(&I2CD1, slaveaddr, txbuffer, txbytes, rxbuf, rxbytes, TIMEOUT);
+    chThdSleepMilliseconds(500); //estudar valor real e ver se deve ser empirico esse resultado
+    chprintf((BaseSequentialStream *) &SD1, "debug = %s\r\n",debug);
+    chprintf((BaseSequentialStream *) &SD1, "final da execucao da thread\r\n");
+      }
 }
+
+
 
 /*
  * Application entry point.
@@ -45,18 +72,24 @@ int main(void) {
    */
   halInit();
   chSysInit();
-  i2cInit();  
+  i2cInit();
   /*
    * Activates the serial driver 1 using the driver default configuration.
    */
   sdStart(&SD1, NULL);
 
-  /*
-   * Starts the LED blinker thread.
-   */
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 
-  chnWrite(&SD1, (const uint8_t *)"Hello World!\r\n", 14);
+
+
+  /*chnWrite
+   * Starts the MasterSendI2C thread
+   */
+//  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+  chnWrite(&SD1, (const uint8_t *)"Ler configuracoes do I2C do teclado\r\n",14);
+
+chThdSleepMilliseconds(1000);
+ chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 
   while (TRUE) {
     chThdSleepMilliseconds(1000);
